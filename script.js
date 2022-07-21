06842c5a136dace066b3a2a183b39a2c04959775
Trevor Brownlee <t.brownlee@yahoo.com>
2022-07-21 10:49:50 -0400

    Created functions to get json data

--- script.js
+++ script.js
@@ -1,85 +1,124 @@
 "use strict"
 
 
+async function getData() {
+    let url = "data.json";
+    try {
+        let response = await fetch(url);
+        return await response.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-let days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
-let amounts = [17.45, 34.91, 52.36, 31.07, 23.39, 43.28, 25.48];
 
+async function renderData() {
+    let spendingData = await getData();
+    
+    let days = [];
+    let amounts = [];
+
+    spendingData.forEach(data => {
+        days.push(data.day);
+        amounts.push(data.amount);
+    })
+
+    createChart(days, amounts);
+}
+
+
+
+renderData();
+
+
+
+
+
+
+/* Colors */
 let softRed = "hsl(10, 79%, 65%)";
+let cyan = "hsl(186, 34%, 60%)";
+let fadedSoftRed = "hsl(10, 79%, 75%)";
+let fadedCyan = "hsl(186, 34%,70%)";
+
 let mediumBrown = "hsl(28, 10%, 53%)";
 let darkBrown = "hsl(25, 47%, 15%)";
 
 
-const ctx = document.getElementById('canvas').getContext('2d');
-const myChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: days,
-    datasets: [{
-      data: amounts,
-      barPercentage: .96,
-      borderWidth: 1,
-      borderRadius: 5,
-      backgroundColor: [
-        'hsl(10, 79%, 65%)',
-        'hsl(10, 79%, 65%)',
-        'hsl(186, 34%, 60%)',
-        'hsl(10, 79%, 65%)',
-        'hsl(10, 79%, 65%)',
-        'hsl(10, 79%, 65%)',
-        'hsl(10, 79%, 65%)',
-      ],
-      hoverBackgroundColor: [
-        'hsl(10, 79%, 75%)',
-        'hsl(10, 79%, 75%)',
-        'hsl(186, 34%,70%)',
-        'hsl(10, 79%, 75%)',
-        'hsl(10, 79%, 75%)',
-        'hsl(10, 79%, 75%)',
-        'hsl(10, 79%, 75%)',
-      ],
-    }]
-  },
-  options: {
-    maintainAspectRatio: false,
-    onHover: (event, chartElement) => {
-      event.native.target.style.cursor = chartElement[0]
-        ? 'pointer'
-        : 'default';
-    },
-    scales: {
-      y: {
-        display: false
-      },
-      x: {
-        grid: {
-          display: false,
-          drawBorder: false
+
+
+function createChart(days, amounts) {
+    const canvas = document.getElementById("canvas").getContext("2d");
+    const myChart = new Chart(canvas, {
+        type: "bar",
+        data: {
+            labels: days,
+            datasets: [{
+                data: amounts,
+                barPercentage: .96,
+                borderWidth: 1,
+                borderRadius: 5,
+                backgroundColor: [
+                    softRed,
+                    softRed,
+                    cyan,
+                    softRed,
+                    softRed,
+                    softRed,
+                    softRed,
+                ],
+                hoverBackgroundColor: [
+                    fadedSoftRed,
+                    fadedSoftRed,
+                    fadedCyan,
+                    fadedSoftRed,
+                    fadedSoftRed,
+                    fadedSoftRed,
+                    fadedSoftRed,
+                ],
+            }]
         },
-        ticks: {
-          padding: 0,
-          color: mediumBrown,
-          font: {
-            family: "DM Sans"
-          }
+        options: {
+            maintainAspectRatio: false,
+            onHover: (event, chartElement) => {
+                event.native.target.style.cursor = chartElement[0]
+                    ? "pointer"
+                    : "default";
+            },
+            scales: {
+                y: {
+                    display: false
+                },
+                x: {
+                    grid: {
+                        display: false,
+                        drawBorder: false
+                    },
+                    ticks: {
+                        padding: 0,
+                        color: mediumBrown,
+                        font: {
+                            family: "DM Sans"
+                        }
+                    }
+                },
+            },
+            plugins: {
+                legend: {
+                    display: false,
+                },
+                tooltip: {
+                    caretSize: 0,
+                    yAlign: "bottom",
+                    xAlign: "center",
+                    displayColors: false,
+                    backgroundColor: darkBrown,
+                    caretPadding: 5,
+                    callbacks: {
+                        title: function () { },
+                    }
+                }
+            }
         }
-      },
-    },
-    plugins: { 
-      legend: { 
-        display: false,
-      },
-      tooltip: {
-        caretSize: 0,
-        yAlign: "bottom",
-        xAlign: "center",
-        displayColors: false,
-        backgroundColor: darkBrown,
-        caretPadding: 5,
-        callbacks: {
-          title: function() {},
-       }
-      }
-    }
-  }
-});
+    });
+}
